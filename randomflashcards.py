190538181b2d63e43afd910730a2ddf5cc8dd4eb
MichaelOdumosu57 <modumosu@albany.edu>
2016-10-22 15:21:28 -0400

    Add files via upload
    Here Ive made the flashcard class objects, but I have not completed the randomization and quiz parts to it

---
+++ randomflashcards.py
@@ -0,0 +1,60 @@
+from random import *
+class Flashcards:
+
+    class Card:
+
+        def __init__():
+
+            self.term = ""
+            self.description= ""
+        
+    
+
+    def __init__(self):
+
+        self.terms = []
+        self.descriptions = []
+    def input(self):
+
+        term = input("What is the term")
+        description = input("What is the description")
+        term = card
+
+    def random(self):
+        
+
+        dictictionary = dict()
+
+        for i in self.terms:
+            dictionary[i]
+
+        for j in self.dictionary:
+            dictionary[j] = j
+
+        inquestion = dictionary.choice(77)
+
+        return inquestion
+
+
+Archaelogy = Flashcards()
+
+for i in range(7):
+    terms = input("Please input terms")
+    definition = input("Please input definition")
+
+    Archaelogy.terms.append(terms)
+    Archaelogy.terms.append(definition)
+
+query = input("Please type in input and definition")
+    
+
+
+        
+        
+    
+    
+
+        
+
+
+        
