fbd523df5167b8585aadace9af0e4cf4cf33a36f
MichaelOdumosu57 <modumosu@albany.edu>
2016-10-24 22:47:13 -0400

    Add files via upload
    This change removes the Card subclass which is unnecessary since terms and defintions can be put directly into the table

--- randomflashcards.py
+++ randomflashcards.py
@@ -1,12 +1,6 @@
 from random import *
 class Flashcards:
 
-    class Card:
-
-        def __init__():
-
-            self.term = ""
-            self.description= ""
         
     def __init__(self):
 
@@ -39,13 +33,8 @@ for i in range(7):
     terms = input("Please input terms")
     definition = input("Please input definition")
 
-    Archaelogy.Card.term = terms
-    Archaelogy.Card.description = definition
+    Archaelogy.flashcards[terms] =Archaelogy.Card.definition
     
-    Archaelogy.flashcards[Archaelogy.Card.term] =Archaelogy.Card.description
-    
-    Archaelogy.Card.term = ""
-    Archaelogy.Card.description = ""
     
 
 query = input("Please type in input and definition")
