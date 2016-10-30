3ae54f9d7ac74140c8113d1f5122547a50c64de8
MichaelOdumosu57 <modumosu@albany.edu>
2016-10-30 17:30:43 -0400

    Add files via upload

--- randomflashcards.py
+++ randomflashcards.py
@@ -5,26 +5,15 @@ class Flashcards:
     def __init__(self):
 
         self.flashcards = dict()
+
+    def play_a_game(self):
+
+        print("Lets play Flashcards and Your Mind!!!")      
+        for i in self.flashcards.keys():
+            print("What is" + i + "?")
+
+
         
-    def input(self):
-
-        term = input("What is the term")
-        description = input("What is the description")
-        term = card
-
-    def random(self):
-        
-        dictictionary = dict()
-
-        for i in self.terms:
-            dictionary[i]
-
-        for j in self.dictionary:
-            dictionary[j] = j
-
-        inquestion = dictionary.choice(77)
-
-        return inquestion
 
 
 Archaelogy = Flashcards()
@@ -32,21 +21,36 @@ Archaelogy = Flashcards()
 for i in range(7):
     terms = input("Please input terms  ")
     definition = input("Please input definition   ")
-
     Archaelogy.flashcards[terms] =definition
-
     answer= input("Are you done with the flashcard  ")
+    if answer == "yes":break
+
+then= input("What would you like to do next?")
+if then.find("play") != -1:
+    Archaelogy.play_a_game()
+
+if then.find("study") != -1:
+    Archaelogy.reg_studY()
+
+
+
+        
     
     
 while True:
                   
     query = input("Please type in term  ")
-
+    a=0
+    
     for i in Archaelogy.flashcards.keys():
 
         if i == query:
             print(Archaelogy.flashcards[i])
+            a=1
             break
+    if a==0:
+        print("Could not find search term, would you like to try another?")
+
                   
     
     
