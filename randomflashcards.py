45c747b97a404e27ae9fa70ab6f6c15fc4008fa6
MichaelOdumosu57 <modumosu@albany.edu>
2016-12-14 22:35:56 -0500

    Add files via upload
    Really completes the program the game is simple however

--- randomflashcards.py
+++ randomflashcards.py
@@ -15,6 +15,7 @@ class Flashcards:
 
         while answer_i != "q":
             print("If you want to quit at any time press q")
+            time.sleep(3)
             
             for i in self.flashcards.keys():
                 print("What is" + i + "?")
@@ -58,23 +59,69 @@ class Flashcards:
                     break
             if a==0:
                 print("Could not find search term, would you like to try another?")
+
+    def add(self,term = None):
+        try:
+            while True:
+                answer = input("Do you want to add again?")
+                if answer == "no":
+                    print(str(self.flashcards))
+                    return
+                terms = str(input("Please input terms  "))
+                definition = str(input("Please input definition   "))
+                self.flashcards[terms] =definition 
+        except:
+            print("Place quotes")
+            self.add()
+
+    def save(self):
+
+        final = open("fina.txt","w")
+        for i,j in self.flashcards.items():
+            i += "+"
+            final.write(i)
+            final.flush()
+            final.write(j)
+            final.flush()
+            final.write("\n")
+            final.flush()
+        final.close()
+
+    def load(self):
+        final = open("fina.txt","r")
+        flash = final.read().splitlines()
+        for i in flash:
+            print(len(flash))
+        for i in flash:
+            i  = i.split("+")
+            self.flashcards[i[0]]= i[1]
+        print(str(self.flashcards))
+        final.close()
+        return 
+
+
+def main():
+        Archaelogy = Flashcards()
+        while True:
+
+            then= input("What would you like to do next?")
+            if then.find("play") != -1:
+                Archaelogy.play_a_game()
+
+            if then.find("study") != -1:
+                Archaelogy.reg_studY()
+
+            if then.find("add") != -1:
+                Archaelogy.add()
+
+            if then.find("save") != -1:
+                Archaelogy.save()
                             
-Archaelogy = Flashcards()
+            if then.find("load") != -1:
+                Archaelogy.load()
 
-for i in range(7):
-    terms = input("Please input terms  ")
-    definition = input("Please input definition   ")
-    Archaelogy.flashcards[terms] =definition
-    answer= input("Are you done with the flashcard  ")
-    if answer == "yes":break
 
-then= input("What would you like to do next?")
-if then.find("play") != -1:
-    Archaelogy.play_a_game()
-
-if then.find("study") != -1:
-    Archaelogy.reg_studY()
-
+main()
 
 
         
